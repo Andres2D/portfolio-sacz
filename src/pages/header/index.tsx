4b59cd1aae6bf8376f4db8e59c42c6768d45fd35
Andres2D <andres2d1997.1206@gmail.com>
2024-02-26 18:27:11 -0500

    Add base design for header

--- src/pages/header/index.tsx
+++ src/pages/header/index.tsx
@@ -1,30 +1,32 @@
 import * as React from "react";
 import * as styles from './index.module.scss';
+import {contactLinks, sectionsList} from '../../constants';
 
 const HeaderComponent: React.FC = () => {
+
+  const linksMap = sectionsList.map(section => <a className={styles.link} href="#">{section}</a>);
+  const contactMap = contactLinks.map(contact => <a href="">{contact}</a>);
+
   return (
     <header className={`${styles.header}`}>
 
-      <h1 className={styles.title}>Sebastián Alcaraz</h1>
-      <h3 className={styles.subtitle}>Digital Marketing specialist</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus aperiam tempora ipsa quae. Eaque optio hic suscipit animi autem eum. Placeat aliquam obcaecati eius dolorum rerum iure possimus vitae impedit?</p>
+      <div>
+        <h1 className={styles.title}>Sebastián Alcaraz</h1>
+        <h3 className={styles.subtitle}>Digital Marketing specialist</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus aperiam tempora ipsa quae. Eaque optio hic suscipit animi autem eum. Placeat aliquam obcaecati eius dolorum rerum iure possimus vitae impedit?
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus blanditiis modi repellendus explicabo officia impedit quidem eligendi, nihil excepturi vel, sit, nemo aspernatur! Fuga vel incidunt facilis qui autem nobis.
+        </p>
+      </div>
       
       <div className={styles.links}>
-        <a href="#">About</a>
-        <a href="#">Experience</a>
-        <a href="#">Projects</a>
+        {linksMap}
       </div>
 
       <div className={styles.contact}>
-        <a href="">LinkedIn</a>
-        <a href="">Facebook</a>
-        <a href="">Canvas</a>
-        <a href="">Spotify</a>
-        <a href="">Gmail</a>
+        {contactMap}
       </div>
 
     </header>
   );
-}
-
+};
 export default HeaderComponent;
