a488c741a23e7fc52a951d1b8e4ae10001646fda
Andres2D <andres2d1997.1206@gmail.com>
2024-02-27 00:22:08 -0500

    Add icons assets

---
+++ src/images/svg/index.ts
@@ -0,0 +1,15 @@
+import facebook from './facebook.svg';
+import gmail from './gmail.svg';
+import instagram from './instagram.svg';
+import linkedin from './linkedin.svg';
+import twitter from './twitter-x.svg';
+import whatsapp from './whatsapp.svg';
+
+export {
+  facebook,
+  gmail,
+  instagram,
+  linkedin,
+  twitter,
+  whatsapp
+};
