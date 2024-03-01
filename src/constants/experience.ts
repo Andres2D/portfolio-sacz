9e295c09038a6a2cac95a4316508887c1ccb5e66
Andres2D <andres2d1997.1206@gmail.com>
2024-03-01 18:25:37 -0500

    Add experience information

--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -1,32 +1,82 @@
 import { Experience } from "../interfaces/experience";
 
 export const experience: Experience[] = [
-  {
+  { 
     id: 'A1',
-    jobTitle: 'Marketing especialist',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid beatae consequuntur quia earum voluptas aut velit reprehenderit! Esse qui, voluptate adipisci autem perferendis molestiae error repudiandae molestias, natus hic sed.',
-    initialDate: '2024-01-25',
-    tags: ['SEO', 'Google analytics', 'Public relations', 'Sales', 'Customer service']
+    jobTitle: 'Social Media Management / Content creator',
+    place: 'The RV Farm • vaguhan, Ontario',
+    description: [
+      'Developed engaging multi-channel audiovisual content and marketing strategies to increase online presence and awareness, which led to an increase in sales of 35% in the first month.',
+      'Managed different social media platforms, implemented proper CRM strategies, and increased sales and in-store traffic.',
+      'I supported business development efforts and inventory management.',
+      'Developed relationships with prospects and previous buyers to improve customer loyalty.',
+      'Develop original content through social listening and independent sources.',
+    ],
+    initialDate: 'Jan 2024',
+    tags: ['Social media', 'Content creator', 'Sales', 'Management'],
+    redirectUrl: 'https://thervfarm.ca/'
   },
-  {
+  { 
     id: 'A2',
-    jobTitle: 'Marketing especialist',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid beatae consequuntur quia earum voluptas aut velit reprehenderit! Esse qui, voluptate adipisci autem perferendis molestiae error repudiandae molestias, natus hic sed.',
-    initialDate: '2024-01-25',
-    tags: ['SEO', 'Google analytics', 'Public relations', 'Sales', 'Customer service']
+    jobTitle: 'Social Media Coordinator',
+    place: 'Ginko Health Store • Toronto, Ontario',
+    description: [
+      'Supported SEO initiatives to improve content, keywords and branding.',
+      'Increased customer engagement through social media.',
+      'I wrote social media content to increase engagement with customers.',
+      'Create social media content with consistent content and tone.',
+      'Developed comprehensive resource management reports, data presentations, and data analytics reports.',
+    ],
+    initialDate: 'Aug 2023',
+    finalDate:'Jan 2024',
+    tags: ['SEO', 'Branding', 'Social media', 'Content creator', 'Reports', 'Analytics'],
+    redirectUrl: 'https://ginkgohealthshop.com/'
   },
-  {
+  { 
     id: 'A3',
-    jobTitle: 'Marketing especialist',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid beatae consequuntur quia earum voluptas aut velit reprehenderit! Esse qui, voluptate adipisci autem perferendis molestiae error repudiandae molestias, natus hic sed.',
-    initialDate: '2024-01-25',
-    tags: ['SEO', 'Google analytics', 'Public relations', 'Sales', 'Customer service']
+    jobTitle: 'Marketing specialist',
+    place: 'The Vault Inc. • Vaughan, Ontario',
+    description: [
+      'Increased customer engagement through social media.',
+      'Analyzed and reported social media and online marketing campaign results.',
+      `Monitored the online presence of the company's brand to engage with users and strengthen customer relationships.`,
+      'Designed and implemented social media strategies to align with business goals.',
+      `Monitored the online presence of the company's brand to engage with users and strengthen', customer relationships.`
+    ],
+    initialDate: 'Aug 2023',
+    finalDate: 'Dec 2023',
+    tags: ['Social media', 'Campaigns', 'Designer'],
   },
-  {
+  { 
     id: 'A4',
-    jobTitle: 'Marketing especialist',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid beatae consequuntur quia earum voluptas aut velit reprehenderit! Esse qui, voluptate adipisci autem perferendis molestiae error repudiandae molestias, natus hic sed.',
-    initialDate: '2024-01-25',
-    tags: ['SEO', 'Google analytics', 'Public relations', 'Sales', 'Customer service']
-  }
+    jobTitle: 'Marketing specialist Internship',
+    place: 'Staff Your Event • Toronto, Ontario',
+    description: [
+      'Create engaging content marketing initiatives to drive traffic, increasing contract opportunities',
+      'Monitored schedules to verify project completion on schedule and within approved cost limitations',
+      'Built and kept strong relationships with loyal customers',
+      'Analyzed social media engagement to identify high-performing campaigns for scalability, increasing brand reach by 27%.',
+      'Maintained a complete database of files, contacts, and project materials.',
+      'Collaborated with team members to help expand marketing channels',
+    ],
+    initialDate: 'May 2023',
+    finalDate: 'Jul 2023',
+    tags: ['Social media', 'Branding', 'Marketing'],
+    redirectUrl: 'http://www.staffyourevent.ca/'
+  },
+  { 
+    id: 'A5',
+    jobTitle: 'Bilingual Customer Service Representative',
+    place: 'Walmart • Toronto, Ontario',
+    description: [
+      'I achieved a high performance of 4.8+ stars with a number of positive reviews',
+      'Built the logic for a streamlined ad-serving platform that scaled',
+      'I specialized in customer relations and delivered professional customer service in both English and Spanish.',
+      'Communicated with management when customer issues escalated and worked to find resolutions.',
+    ],
+    initialDate: 'Jun 2021',
+    finalDate: 'Jan 2022',
+    tags: ['Customer support', 'Performance'],
+    redirectUrl: 'https://www.walmart.ca/en'
+  },
 ];
