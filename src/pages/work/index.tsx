657b2b7e4ed47ec89c02dfe861d01076c58380cd
Andres2D <andres2d1997.1206@gmail.com>
2024-03-05 18:19:58 -0500

    Add motion to work section

--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -1,21 +1,29 @@
 import * as React from "react";
+import { motion } from 'framer-motion';
 import { workList } from '../../constants/work';
 import WorkCardComponent from "./components/work-card";
 import * as styles from './index.module.scss';
+import { textVariant, staggerContainer } from '../../utils/motion';
 
 const WorkComponent: React.FC = () => {
 
-  const workMap = workList.map(work => {
-    return <WorkCardComponent key={work.id} work={work}/>
-  });
+  const workMap = workList.map((work, index) => (
+    <WorkCardComponent key={work.id} index={index} work={work}/>
+  ));
 
   return (
-    <div id='work' className={styles.container}>
-      <h3 className={styles.title}>My work</h3>
+    <motion.div 
+      id='work' 
+      className={styles.container}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: true, amount: 0.15 }}
+      variants={{...staggerContainer()} }>
+      <motion.h3 className={styles.title} variants={textVariant}>My work</motion.h3>
       <section className={styles.section}>
         {workMap}
       </section>
-    </div>
+    </motion.div>
   );
 };
 export default WorkComponent;
