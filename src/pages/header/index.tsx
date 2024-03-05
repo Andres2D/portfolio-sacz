ab0936d240c306f6beffa0aa0cfe6755a14ce972
Andres2D <andres2d1997.1206@gmail.com>
2024-03-05 18:06:14 -0500

    Add motion variants to header component

--- src/pages/header/index.tsx
+++ src/pages/header/index.tsx
@@ -1,21 +1,29 @@
 import * as React from "react";
+import { motion } from 'framer-motion';
 import * as styles from './index.module.scss';
 import {contactLinks, sectionsList} from '../../constants';
 import { Contact } from '../../interfaces/header';
 import { profile } from '../../constants/header';
 import { resume } from "../../files";
+import { staggerContainer, textVariant, fadeIn } from '../../utils/motion';
 
 const HeaderComponent: React.FC = () => {
 
-  const linksMap = sectionsList.map(section => 
-    <a className={styles.link} href={`#${section.reference}`}>{section.label}</a>
+  const linksMap = sectionsList.map((section, index) => 
+    <motion.a 
+      className={styles.link} 
+      href={`#${section.reference}`}
+      variants={fadeIn('left', 'spring', index * 0.2, 1)}>
+        {section.label}
+    </motion.a>
   );
-  const contactMap = contactLinks.map(contact => 
-    <img 
+  const contactMap = contactLinks.map((contact, index) => 
+    <motion.img 
       className={styles.icon}
       src={contact.image}
       alt={contact.label}
       onClick={() => redirect(contact)}
+      variants={fadeIn('up', 'spring', index * 0.2, 1)}
     />
   );
 
@@ -28,25 +36,31 @@ const HeaderComponent: React.FC = () => {
     }
   };
 
+  
   return (
-    <header className={`${styles.header}`}>
+    <motion.header
+      variants={{...staggerContainer(), ...textVariant} }
+      className={styles.header}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: true, amount: 0.15 }}
+      >
+        <div>
+          <h1 className={styles.title}>{profile.name}</h1>
+          <h3 className={styles.subtitle}>{profile.role}</h3>
+          <p>{profile.description}</p>
+          <a href={resume} target="_blank" className={styles.download}>Download resume</a>
+        </div>
+        
+        <div className={styles.links}>
+          {linksMap}
+        </div>
 
-      <div>
-        <h1 className={styles.title}>{profile.name}</h1>
-        <h3 className={styles.subtitle}>{profile.role}</h3>
-        <p>{profile.description}</p>
-        <a href={resume} target="_blank" className={styles.download}>Download resume</a>
-      </div>
-      
-      <div className={styles.links}>
-        {linksMap}
-      </div>
-
-      <div className={styles.contact}>
-        {contactMap}
-      </div>
-
-    </header>
+        <div className={styles.contact}>
+          {contactMap}
+        </div>
+    </motion.header>
   );
 };
+
 export default HeaderComponent;
